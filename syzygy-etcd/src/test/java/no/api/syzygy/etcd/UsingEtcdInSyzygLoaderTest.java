3eb3a8f030a4ad0c2e8684a8a817ba23cc17133f
Erlend <erlend@api.no>
2015-01-22 14:44:11 +0100

    DEVOPS-376  Adding a litte test output.

--- syzygy-etcd/src/test/java/no/api/syzygy/etcd/UsingEtcdInSyzygLoaderTest.java
+++ syzygy-etcd/src/test/java/no/api/syzygy/etcd/UsingEtcdInSyzygLoaderTest.java
@@ -1,5 +1,6 @@
 package no.api.syzygy.etcd;
 
+import no.api.syzygy.SyzygyHelper;
 import no.api.syzygy.loaders.SyzygyLoader;
 import org.junit.After;
 import org.junit.Assume;
@@ -8,8 +9,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -48,6 +51,10 @@ public class UsingEtcdInSyzygLoaderTest {
         assertEquals("fallback value 2", syzygy.lookup("key2"));
         assertEquals("etcd_c", syzygy.lookup("key3"));
         assertEquals("fallback value 4", syzygy.lookup("key4"));
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        SyzygyHelper.printConfigTo(syzygy.listAllProperties(), new OutputStreamWriter(baos));
+        log.debug("\n" + baos.toString());
 
         assertTrue(etcd.remove("key1"));
         assertTrue(etcd.remove("key3"));

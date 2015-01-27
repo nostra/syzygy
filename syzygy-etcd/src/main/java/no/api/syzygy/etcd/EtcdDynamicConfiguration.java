2121a3a125bbb94c4f30b82b2a001431b8ef353e
Erlend <erlend@api.no>
2015-01-27 09:42:30 +0100

    DEVOPS-374 using global variable for etcd location (as default)

--- syzygy-etcd/src/main/java/no/api/syzygy/etcd/EtcdDynamicConfiguration.java
+++ syzygy-etcd/src/main/java/no/api/syzygy/etcd/EtcdDynamicConfiguration.java
@@ -6,7 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * This dynamic configuration takes 2 arguments:
+ * <ul>
+ *     <li><tt>global_etcd_url</tt>: For where etcd reside. Defaulting to localhost. You can also use refkey+_etcd_url</li>
+ *     <li>refkey+<tt>_etcd_prefix</tt> : the subdirectory on which you attach this instance. It looks
+ *     something like this: /syzygy/something </li>
+ * </ul>
  */
 public class EtcdDynamicConfiguration implements SyzygyDynamicLoader {
     private static final Logger log = LoggerFactory.getLogger(EtcdDynamicConfiguration.class);
@@ -14,6 +19,9 @@ public class EtcdDynamicConfiguration implements SyzygyDynamicLoader {
     @Override
     public SyzygyConfig createSyzygyConfigWith(String refkey, SyzygyConfig loaderConfiguration) {
         String etcdUrl = loaderConfiguration.lookup(refkey+"_etcd_url");
+        if ( etcdUrl == null ) {
+            etcdUrl = loaderConfiguration.lookup("global_etcd_url");
+        }
         String prefix = loaderConfiguration.lookup(refkey+"_etcd_prefix");
         if ( etcdUrl == null ) {
             etcdUrl = "http://127.0.0.1:4001/v2/";

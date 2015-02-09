ad91eb87a5159f0531e3c2bd507c259c7d405c3e
Erlend <erlend@api.no>
2015-02-09 15:43:54 +0100

    DEVOPS-420 Added framework code for running syzygy application

--- syzygy-service/src/main/java/no/api/syzygy/service/SyzygyApplication.java
+++ syzygy-service/src/main/java/no/api/syzygy/service/SyzygyApplication.java
@@ -1,22 +1,34 @@
 package no.api.syzygy.service;
 
+import no.api.syzygy.etcd.EtcdConnector;
+import no.api.syzygy.loaders.SyzygyFileConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 public class SyzygyApplication {
     private static final Logger log = LoggerFactory.getLogger(SyzygyApplication.class);
     public static void main(String[] args) {
         SyzygyApplication app = new SyzygyApplication();
         switch (args.length) {
-            case 1:
-                new SyzygyApplication().doMojo();
-                break;
-            case 2:
+            case 3:
+                try {
+                    List<String> result = new SyzygyApplication().doMojo(args[0],args[1],args[2]);
+                    for ( String str : result ) {
+                        log.info(str);
+                    }
+                } catch (Exception e ) {
+                    log.error("Execution ended with exception.", e);
+                    System.exit(1);
+                }
                 break;
             default:
                 app.instructions();
         }
-
+        log.info("Execution finished");
 
     }
 
@@ -24,15 +36,26 @@ public class SyzygyApplication {
         System.out.println("You need to supply 2 arguments: ");
         System.out.println("  path/to/file.yaml");
         System.out.println("  target URL for etcd");
-        System.out.println("  etcd mount point, usually filename. It becomes /syzygy/config/");
-
+        System.out.println("  etcd mount point, usually filename. It gets prefixed with /syzygy/");
+        System.out.println("");
         System.out.println("Example:");
         System.out.println("   path/to/config.yaml ");
         System.out.println("   http://127.0.0.1:4001/v2/ ");
         System.out.println("   config");
     }
 
-    private void doMojo() {
+    private List<String> doMojo(String pathToFile, String URLToEtcd, String mountName) {
+        SyzygyFileConfig sfc = new SyzygyFileConfig(mountName).load(new File(pathToFile));
+        Map<String, Object> syzygyMap = sfc.getMap();
+        EtcdConnector etcd = null;
+        try {
+            etcd = EtcdConnector.attach(URLToEtcd, "/syzygy/");
+            return etcd.syncMapInto(mountName + "/", syzygyMap);
+        } finally {
+            if (etcd != null) {
+                etcd.stop();
+            }
+        }
 
     }
 }

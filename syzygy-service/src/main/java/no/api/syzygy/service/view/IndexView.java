f60614a27e36337e0d5656be186ab8c0596cd712
Erlend Nossum <erlend@api.no>
2015-02-12 17:20:48 +0100

    DEVOPS-424 Adding index view

---
+++ syzygy-service/src/main/java/no/api/syzygy/service/view/IndexView.java
@@ -0,0 +1,13 @@
+package no.api.syzygy.service.view;
+
+import io.dropwizard.views.View;
+
+/**
+ *
+ */
+public class IndexView extends View {
+
+    public IndexView() {
+        super("index.ftl");
+    }
+}

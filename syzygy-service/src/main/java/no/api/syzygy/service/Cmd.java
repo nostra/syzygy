7ace0e9c6c98ceac6c56d8e752579fc763004d60
Erlend Nossum <erlend@api.no>
2015-02-16 14:11:04 +0100

    DEVOPS-426 Working on sending parameters to command

--- syzygy-service/src/main/java/no/api/syzygy/service/Cmd.java
+++ syzygy-service/src/main/java/no/api/syzygy/service/Cmd.java
@@ -16,13 +16,14 @@ public class Cmd extends ConfiguredCommand<SyzygyConfiguration> {
     private static final Logger log = LoggerFactory.getLogger(Cmd.class);
 
     protected Cmd() {
-        super("ls", "This is just some description");
+        super("etcdsync", "Synchronize a file to etcd");
     }
 
     @Override
     public void configure(Subparser subparser) {
-        log.warn("Configure " + subparser);
-        subparser.addArgument("-x");
+        log.warn("Configure method");
+        subparser.addArgument("-f", "--file").help("Full path to yamlfile to transfer.");
+        subparser.addArgument("-t", "--target").help("etcd mount point, usually the filename. It gets prefixed with /syzygy/");
     }
 
     @Override
@@ -32,9 +33,8 @@ public class Cmd extends ConfiguredCommand<SyzygyConfiguration> {
             final JsonLogger jsonLogger = new JsonLogger(config.getJsonLogPath());
             jsonLogger.attach();
         }
-
-        log.warn("Running! Whee! NS:" + namespace.getString("x"));
-        log.info("This line is on info level");
+        log.warn("Running! Whee! NS:" + namespace.getString("t"));
+        log.info("This line is on info level. Json log path: "+config.getJsonLogPath());
         log.debug("This line is on debug level");
     }
 
